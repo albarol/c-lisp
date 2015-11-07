1e1d41a7f7e7fe0d72cad2ab381b829732ff605b
fakeezz <fakeezz@gmail.com>
2015-11-07 10:44:05 -0200

    parser lisp tokens

--- repl.c
+++ repl.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 
+#include <mpc.h>
 #include <editline/readline.h>
 #include <editline/history.h>
 
@@ -11,17 +10,38 @@ int main(int argc, char** argv) {
     puts("Lisp version 0.0.1\n");
     puts("Press Ctrl+c to Exit\n");
 
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lisp = mpc_new("lisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                        \
+        number   : /-?[0-9]+/;                               \
+        operator : '+' | '-' | '*' | '/' ;                   \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;   \
+        lisp     : /^/ <operator> <expr>+ /$/ ;              \
+    ",
+    Number, Operator, Expr, Lisp);
+
     while (1) {
 
         char* input = readline("lisp> ");
-
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lisp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.output);
+            mpc_err_delete(r.output);
+        }
 
         free(input);
-
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lisp);
 
     return 0;
 
