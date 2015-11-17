cd2415cd0a88b6745cb1c51022f7b455d0fcf097
fakeezz <fakeezz@gmail.com>
2015-11-17 21:38:31 -0200

    improved error handle

--- builtin.c
+++ builtin.c
@@ -100,8 +100,7 @@ clisp_builtin_list_create(clisp_env_t* env, clisp_token_t* token) {
 
 clisp_token_t*
 clisp_builtin_list_head(clisp_env_t* env, clisp_token_t* token) {
-    clisp_assert(token, token->count == 1,
-                 "Function 'head' passed too many arguments!");
+    clisp_assert_count(token, 1);
 
     clisp_token_t* list = token->tokens[0];
     clisp_assert(token, list->type == TOKEN_QEXPRESSION,
@@ -119,8 +118,8 @@ clisp_builtin_list_head(clisp_env_t* env, clisp_token_t* token) {
 
 clisp_token_t*
 clisp_builtin_list_tail(clisp_env_t* env, clisp_token_t* token) {
-    clisp_assert(token, token->count == 1,
-                 "Function 'tail' passed too many arguments!");
+    clisp_assert_count(token, 1);
+
     clisp_token_t* list = token->tokens[0];
     clisp_assert(token, list->type == TOKEN_QEXPRESSION,
                  "Function 'tail' passed incorrect types");
@@ -137,8 +136,7 @@ clisp_builtin_list_join(clisp_env_t* env, clisp_token_t* token) {
 
     for (int i = 0; i < token->count; i++) {
         clisp_token_t* child = token->tokens[i];
-        clisp_assert(token, child->type == TOKEN_QEXPRESSION,
-                     "Function 'join' passed incorrect type");
+        clisp_assert_type(token, child->type, TOKEN_QEXPRESSION);
     }
 
     clisp_token_t* child = clisp_token_pop(token, 0);
@@ -154,14 +152,10 @@ clisp_builtin_list_join(clisp_env_t* env, clisp_token_t* token) {
 
 clisp_token_t*
 clisp_builtin_eval(clisp_env_t* env, clisp_token_t* token) {
-    clisp_assert(token, token->count == 1,
-                 "Function 'eval' passed too many arguments! "
-                 "Passed: %li, Expected: %li", token->count, 1);
+    clisp_assert_count(token, 1);
+    clisp_assert_type(token, token->type, TOKEN_QEXPRESSION)
 
     clisp_token_t* child = token->tokens[0];
-    clisp_assert(token, token->type == TOKEN_QEXPRESSION,
-                 "Function 'eval' passed incorrect types "
-                 "Passed: %s, Expected: %s", clisp_print_type_name(token->type), clisp_print_type_name(TOKEN_QEXPRESSION));
 
     child = clisp_token_take(token, 0);
     child->type = TOKEN_QEXPRESSION;
@@ -171,20 +165,17 @@ clisp_builtin_eval(clisp_env_t* env, clisp_token_t* token) {
 
 clisp_token_t*
 clisp_builtin_define(clisp_env_t* env, clisp_token_t* token) {
-    clisp_assert(token, token->tokens[0]->type == TOKEN_QEXPRESSION,
-                 "Function 'def' passed incorrect type "
-                 "Passed: %s, Expected: %s", clisp_print_type_name(token->type), clisp_print_type_name(TOKEN_QEXPRESSION));
 
     clisp_token_t* child = token->tokens[0];
+    clisp_assert_type(token, child->type, TOKEN_QEXPRESSION);
 
     for (int i = 0; i < child->count; i++) {
-        clisp_assert(token, child->tokens[i]->type == TOKEN_SYMBOL,
-                     "Function 'def' cannot define non-symbol");
+        clisp_assert(token, child->tokens[i]->type, TOKEN_SYMBOL)
     }
 
     clisp_assert(token, child->count == token->count-1,
                  "Function 'def' cannot define incorrect "
-                 "number of values to symbols");
+                 "number of values to symbols")
 
     for (int i = 0; i < child->count; i++) {
         clisp_env_put(env, child->tokens[i], token->tokens[i+1]);
--- error.h
+++ error.h
@@ -3,11 +3,13 @@
 
 #include "token.h"
 
-#define clisp_assert(token, cond, fmt, ...) \
-      if (!(cond)) { \
-          clisp_token_t* error = clisp_token_error(fmt, ##__VA_ARGS__); \
-          clisp_token_del(token); \
-          return error; \
-}
+#define clisp_assert(t, cond, fmt, ...) \
+      if (!(cond)) { clisp_token_t* error = clisp_token_error(fmt, ##__VA_ARGS__); clisp_token_del(t); return error; }
+
+#define clisp_assert_count(t, num) \
+    clisp_assert(t, t->count == num, "Incorrect number of arguments. Got: %li, Expected: %li", token->count, num)
+
+#define clisp_assert_type(t, current, expected) \
+    clisp_assert(t, current == expected, "Incorrect type of argument. Got: %s, Expected: %s", clisp_print_type_name(current), clisp_print_type_name(expected))
 
 #endif
