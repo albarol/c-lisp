b96a03e35030461cc0af3364c9ea28bd26f7f62e
fakeezz <fakeezz@gmail.com>
2015-11-26 23:56:16 -0200

    created types file

--- src/token.h
+++ src/token.h
@@ -5,42 +5,8 @@
 #include <string.h>
 #include <stdarg.h>
 
+#include "types.h"
 
-struct clisp_token_t;
-struct clisp_env_t;
-typedef struct clisp_token_t clisp_token_t;
-typedef struct clisp_env_t clisp_env_t;
-
-typedef clisp_token_t* (*clisp_function_t)(clisp_env_t*, clisp_token_t*);
-
-
-typedef enum {
-    TOKEN_ERROR,
-    TOKEN_NUMBER,
-    TOKEN_SYMBOL,
-    TOKEN_STR,
-    TOKEN_FUNCTION,
-    TOKEN_SEXPRESSION,
-    TOKEN_QEXPRESSION
-} clisp_token_type_t;
-
-
-struct clisp_token_t {
-    clisp_token_type_t type;
-
-    float number;
-    char* symbol;
-    char* error;
-    char* str;
-
-    clisp_function_t builtin;
-    clisp_env_t* env;
-    clisp_token_t* formals;
-    clisp_token_t* body;
-
-    int count;
-    clisp_token_t** tokens;
-};
 
 clisp_token_t* clisp_token_number(float number);
 clisp_token_t* clisp_token_error(char* error, ...);
