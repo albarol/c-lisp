23b682448801dc527f68c4fe80ad61b9d105d179
fakeezz <fakeezz@gmail.com>
2016-01-28 19:25:21 -0200

    review bool leaks

--- src/builtin/bool.c
+++ src/builtin/bool.c
@@ -88,7 +88,7 @@ clisp_builtin_bool_compare(clisp_expr_t* expr, clisp_env_t* env, char* op) {
     clisp_expr_assert_count(expr, 2);
 
     clisp_chunk_t* first = clisp_expr_pop(expr, 0);
-    clisp_chunk_t* second = clisp_expr_pop(expr, 0);
+    clisp_chunk_t* second = clisp_expr_take(expr, 0);
 
     int result = 0;
     if (strcmp(op, "eq") == 0) {
@@ -99,8 +99,9 @@ clisp_builtin_bool_compare(clisp_expr_t* expr, clisp_env_t* env, char* op) {
         result = !clisp_chunk_cmp(first, second);
     }
 
-    clisp_expr_free(expr);
-    return clisp_chunk_bool(result);
+    clisp_chunk_free(first);
+    clisp_chunk_free(second);
+    return clisp_chunk_bool((bool)result);
 }
 
 clisp_chunk_t*
